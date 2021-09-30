6bff1f4603470ed1af9be39cdc482684abbb8ea7
ag-noel <a.g.noel@live.ca>
2021-09-30 19:07:08 -0400

    Freshwater Fly progress

--- js/freshwaterfly.js
+++ js/freshwaterfly.js
@@ -1,9 +1,10 @@
 var cards = [
-    "HIT!",
-    "MISS",
-    "MISS",
-    "MISS"
-]
+    true,
+    false,
+    false,
+    false
+];
+var drawnCards = [];
 var drawCount = 0;
 
 function rollStrikeDeck()
@@ -19,23 +20,53 @@ function rollStrikeDeck()
     {
         var roll = Math.floor(Math.random() * cards.length);
 
+        // Keep a record of drawn card
+        drawnCards.push(cards[roll]);
+
         // Display the card result
         document.getElementById("strike-result").innerHTML = cards[roll];
 
         // Remove the card so it can't be picked again
         cards.splice(roll, 1);
     }
+
+    if (drawCount > 1)
+    {
+        displayDrawnStrike(drawCount);
+    }
+}
+
+// Displays the previously drawn strikes
+function displayDrawnStrike(index)
+{
+    var idString = "strike-" + (index - 1);
+
+    if (drawnCards[index - 2])
+    {
+        document.getElementById(idString).innerHTML = "O";
+    }
+    else if (!drawnCards[index - 2])
+    {
+        document.getElementById(idString).innerHTML = "X";
+    }
 }
 
 function resetStrikeDeck()
 {
     document.getElementById("strike-result").innerHTML = "Shuffling...";
+
+    // Reset decks and card count
     drawCount = 0;
-    roll = -1;
     cards = [
-        "HIT!",
-        "MISS",
-        "MISS",
-        "MISS"
-    ];
+        true,
+        false,
+        false,
+        false
+    ]
+    drawnCards = []
+
+    // Reset drawn strikes visualisation
+    document.getElementById("strike-1").innerHTML = "--";
+    document.getElementById("strike-2").innerHTML = "--";
+    document.getElementById("strike-3").innerHTML = "--";
 }

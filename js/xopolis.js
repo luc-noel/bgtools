eaf7a2e1e0a4f784e885550d033616e612314a68
luc-noel <a.g.noel@live.ca>
2022-02-02 22:09:11 -0500

    Agro/Sprawlopolis: alphabetising the lists, duh.

--- js/xopolis.js
+++ js/xopolis.js
@@ -1,72 +1,72 @@
 // Livestock order: pig [0], cow [1], chicken [2]
 var agropolis =
     [
-        { name: "Tractor Tours", score: 11, fee: -1, livestock: [2, 2, 1] },
-        { name: "Them Apples", score: 16, fee: -1, livestock: [2, 2, 0] },
+        { name: "Agropolis", score: 18, fee: -1, livestock: [2, 1, 2] },
+        { name: "All the Way Home", score: 5, fee: 0, livestock: [0, 2, 1] },
+        { name: "Bacon and Eggs", score: 14, fee: -1, livestock: [2, 1, 2] },
+        { name: "Big Country", score: 2, fee: -1, livestock: [1, 2, 2] },
+        { name: "Coops and Loops", score: 8, fee: 2, livestock: [0, 2, 0] },
+        { name: "Cornercopia", score: 15, fee: -1, livestock: [0, 2, 2] },
+        { name: "Cornfed", score: 7, fee: 1, livestock: [2, 0, 0] },
+        { name: "Corntry Roads", score: 12, fee: -1, livestock: [2, 0, 2] },
+        { name: "Count your Chickens", score: 4, fee: 2, livestock: [1, 2, 0] },
         { name: "Fruitful Endeavor", score: 6, fee: -1, livestock: [2, 2, 0] },
         { name: "Happy Cows", score: 9, fee: 1, livestock: [2, 0, 1] },
-        { name: "Wine Seller", score: 1, fee: -1, livestock: [1, 2, 2] },
-        { name: "Coops and Loops", score: 8, fee: 2, livestock: [0, 2, 0] },
-        { name: "Big Country", score: 2, fee: -1, livestock: [1, 2, 2] },
-        { name: "Bacon and Eggs", score: 14, fee: -1, livestock: [2, 1, 2] },
-        { name: "All the Way Home", score: 5, fee: 0, livestock: [0, 2, 1] },
-        { name: "Polyominorchards", score: 13, fee: -1, livestock: [2, 0, 2] },
-        { name: "Agropolis", score: 18, fee: -1, livestock: [2, 1, 2] },
-        { name: "Count your Chickens", score: 4, fee: 2, livestock: [1, 2, 0] },
-        { name: "Udderly Impossible", score: 3, fee: 1, livestock: [0, 0, 2] },
-        { name: "Cornfed", score: 7, fee: 1, livestock: [2, 0, 0] },
-        { name: "Cornercopia", score: 15, fee: -1, livestock: [0, 2, 2] },
-        { name: "Corntry Roads", score: 12, fee: -1, livestock: [2, 0, 2] },
         { name: "Noah's Farm", score: 17, fee: -1, livestock: [2, 1, 2] },
-        { name: "Swine Country", score: 10, fee: 0, livestock: [0, 0, 2] }
+        { name: "Polyominorchards", score: 13, fee: -1, livestock: [2, 0, 2] },
+        { name: "Swine Country", score: 10, fee: 0, livestock: [0, 0, 2] },
+        { name: "Them Apples", score: 16, fee: -1, livestock: [2, 2, 0] },
+        { name: "Tractor Tours", score: 11, fee: -1, livestock: [2, 2, 1] },
+        { name: "Udderly Impossible", score: 3, fee: 1, livestock: [0, 0, 2] },
+        { name: "Wine Seller", score: 1, fee: -1, livestock: [1, 2, 2] }
     ]
 
 var combopolis =
     [
-        { name: "Road Trip", score: 7, fee: -1, livestock: [2, 2, 2] },
-        { name: "Petting Zoo", score: 8, fee: -1, livestock: [2, 2, 2] },
-        { name: "Children of the Corn", score: 12, fee: -1, livestock: [2, 2, 2] },
-        { name: "Sour Grapes", score: 13, fee: -1, livestock: [2, 2, 2] },
         { name: "Animal House", score: 14, fee: -1, livestock: [2, 2, 2] },
+        { name: "Children of the Corn", score: 12, fee: -1, livestock: [2, 2, 2] },
+        { name: "Petting Zoo", score: 8, fee: -1, livestock: [2, 2, 2] },
+        { name: "Road Trip", score: 7, fee: -1, livestock: [2, 2, 2] },
+        { name: "Sour Grapes", score: 13, fee: -1, livestock: [2, 2, 2] },
         { name: "Supply and Demand", score: 15, fee: -1, livestock: [2, 2, 2] }
     ]
 
 var sprawlopolis =
     [
-        { name: "Superhighway", score: 12 },
-        { name: "Mini Marts", score: 11 },
-        { name: "Skid Row", score: 15 },
-        { name: "Morning Commute", score: 16 },
-        { name: "The Strip", score: 10 },
-        { name: "The Outskirts", score: 1 },
-        { name: "Master Planned", score: 6 },
-        { name: "The 'Burbs", score: 8 },
-        { name: "Go Green", score: 3 },
-        { name: "Central Perks", score: 7 },
-        { name: "Park Hopping", score: 13 },
-        { name: "Stacks and Scrapers", score: 5 },
-        { name: "Sprawlopolis", score: 18 },
-        { name: "Bloom Boom", score: 2 },
-        { name: "Tourist Traps", score: 17 },
-        { name: "Looping Lanes", score: 14 },
-        { name: "Concrete Jungle", score: 9 },
         { name: "Block Party", score: 4 },
+        { name: "Bloom Boom", score: 2 },
+        { name: "Central Perks", score: 7 },
+        { name: "Concrete Jungle", score: 9 },
+        { name: "Go Green", score: 3 },
+        { name: "Looping Lanes", score: 14 },
+        { name: "Master Planned", score: 6 },
+        { name: "Mini Marts", score: 11 },
+        { name: "Morning Commute", score: 16 },
+        { name: "Park Hopping", score: 13 },
+        { name: "Skid Row", score: 15 },
+        { name: "Sprawlopolis", score: 18 },
+        { name: "Stacks and Scrapers", score: 5 },
+        { name: "Superhighway", score: 12 },
+        { name: "The 'Burbs", score: 8 },
+        { name: "The Outskirts", score: 1 },
+        { name: "The Strip", score: 10 },
+        { name: "Tourist Traps", score: 17 }
     ]
 
 var sprawlopolisBeaches =
     [
-        { name: "Peninsula", score: 0 },
         { name: "Lookout Point", score: 0 },
+        { name: "Peninsula", score: 0 },
         { name: "Scenic Route", score: 0 },
-        { name: "Waterfront", score: 0 },
+        { name: "Waterfront", score: 0 }
     ]
 
 var sprawlopolisConstruction =
     [
-        { name: "Sudden Demolition", score: 0 },
-        { name: "Noisy Neighbours", score: 0 },
         { name: "Bulldozed", score: 0 },
         { name: "Business is Closed", score: 0 },
+        { name: "Noisy Neighbours", score: 0 },
+        { name: "Sudden Demolition", score: 0 }
     ]
 
 /* Variables for game settings */
